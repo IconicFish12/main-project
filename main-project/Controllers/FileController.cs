1875a25b4ddecb43d2f8caf0f98b9da1ca7549ba
Ibnu Syawal Aliefian <superglidingogre0571@gmail.com>
2025-04-22 09:27:43 +0700

    initiating

---
+++ main-project/Controllers/FileController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace main_project.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FileController : ControllerBase
+    {
+        //public Task<ActionResult> uploadFile([FromForm] IFormFile file)
+        //{
+        //    return Ok("hello");
+        //}
+    }
+}
---
+++ main-project/Model/File.cs
@@ -0,0 +1,13 @@
+﻿namespace main_project.Model
+{
+    public class File
+    {
+
+        public string filename { get; set; }
+        public string file_type { get; set; }
+        public int size { get; set; }
+        public string created { get; set; }
+        public string modified { get; set; }
+
+    }
+}
